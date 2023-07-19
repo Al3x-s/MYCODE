01acf3154c81466f355a6e3fdeab836856b67983
Alex <l3xpr1v@proton.me>
2023-07-19 10:28:19 -0500

    dictionary

--- marvel.py
+++ marvel.py
@@ -1,8 +1,7 @@
+from pprint import pprint
 xx = [["Marvel",["Avengers",["Iron Man","Captain America","Thor"]],["X-Men",    ["Wolverine","Cyclops","Storm"]]],["DC Comics",["Justice League",["Superman"    ,"Batman","Wonder Woman"]],["Teen Titans",["Robin","Starfire","Raven", "Cybo    rg", "Beast Boy"]]]]
-  2 super = {}
-  3
-  4 for i in range(len(xx)):
-  5     super[xx[i][0]] = {sub[0]:sub[1] for sub in xx[i][1:] }
-  6
-  7 print(super)
+super = {}
+for i in range(len(xx)):
+    super[xx[i][0]] = {sub[0]:sub[1] for sub in xx[i][1:] }
+pprint(super)
 
