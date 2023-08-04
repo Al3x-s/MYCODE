88fc017b86db9947595b6d8d7065d80c707e206f
Alex <l3xpr1v@proton.me>
2023-08-04 21:23:48 +0000

    fix keygen

--- paramikosftp/sftpmover.py
+++ paramikosftp/sftpmover.py
@@ -5,6 +5,7 @@
 ## import paramiko so we can talk SSH
 import paramiko
 import os
+import shutil
 
 ## where to connect to
 t = paramiko.Transport("10.10.2.3", 22) ## IP and port
@@ -15,10 +16,20 @@ t.connect(username="bender", password="alta3")
 ## Make an sftp connection object
 sftp = paramiko.SFTPClient.from_transport(t)
 
-## iterate across the files within directory
-for x in os.listdir("/home/student/filestocopy/"): # iterate on directory contents
-  if not os.path.isdir("/home/student/filestocopy/"+x): # filter everything that is NOT a directory
-    sftp.put("/home/student/filestocopy/"+x, "/tmp/"+x) # move file to target location
+
+
+# iterate across the files within directory
+
+dest = input("where do you want to move the file exact PTD")
+if not os.path.exists(dest):
+    os.makedirs(dest)
+
+def movethemfiles(xxx,tpath):
+    for x in os.listdir("/home/student/filestocopy/"): # iterate on directory contents
+        if not os.path.isdir("/home/student/filestocopy/"+x): # filter everything that is NOT a directory
+            xxx.put("/home/student/filestocopy/"+x, tpath+x) # move file to target location
+movethemfiles(sftp, dest)
+
 
 ## close the connections
 sftp.close() # close the connection
